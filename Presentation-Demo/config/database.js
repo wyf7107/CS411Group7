35ea1c1f6cdcae6a282d9b974abb378c3bcd08f1
Satoe Sakuma <satoe.sakuma@gmail.com>
2016-12-05 19:47:25 -0500

    Create database.js

---
+++ Presentation-Demo/config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://wyf7107:z8f7c6n9@ds119748.mlab.com:19748/wyf7107_first_test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
